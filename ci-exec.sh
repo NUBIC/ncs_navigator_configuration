4bc388759bb48576dbb87aea0ce18d977f71017b
Rhett Sutphin <rhett@detailedbalance.net>
2011-08-26 10:16:14 -0500

    Fix overnight gemset purge part.
    As with most RVM commands, gemset delete uses non-zero exit codes internally,
    so you need to turn off -e while running it.

--- ci-exec.sh
+++ ci-exec.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -xe
 
 BUNDLER_VERSION=1.0.18
-RVM_GEMSET=ncs_config
+GEMSET=ncs_config
 
-if [ -z $RVM_RUBY ]; then
-    echo "RVM_RUBY must be set"
+if [ -z $CI_RUBY ]; then
+    echo "CI_RUBY must be set"
     exit 1
 fi
 
@@ -15,11 +15,13 @@ set -xe
 
 # On the overnight build, reinstall all gems
 if [ `date +%H` -lt 5 ]; then
-    echo "Purging gemset to verify that all deps still exist"
-    rvm $RVM_RUBY gemset delete $RVM_GEMSET
+    set +xe
+    echo "Purging gemset to verify that all deps can still be installed"
+    rvm --force $CI_RUBY gemset delete $GEMSET
+    set -xe
 fi
 
-RVM_CONFIG="${RVM_RUBY}@${RVM_GEMSET}"
+RVM_CONFIG="${CI_RUBY}@${GEMSET}"
 set +xe
 echo "Switching to ${RVM_CONFIG}"
 rvm use $RVM_CONFIG
