7bb5ad5777d205525cf76dc7e004fbc740d22cd4
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-03-06 11:31:51 -0600

    Add minimally valid hash and add/edit tests for #16, #17, #18, #19.

--- spec/ncs_navigator/configuration_spec.rb
+++ spec/ncs_navigator/configuration_spec.rb
@@ -71,6 +71,18 @@ module NcsNavigator
       }
     }
     let(:from_hash) { Configuration.new(input_hash) }
+    let(:minimum_valid_hash) {
+      {
+        'Staff Portal' => {
+          'uri' => 'https://sp.example.edu/'
+        },
+        'PSC' => {
+          'uri' => 'https://psc.example.edu/'
+        }
+      }
+    }
+    let(:from_minimum_valid_hash) { Configuration.new(minimum_valid_hash) }
+
 
     describe '#initialize' do
       describe 'from an INI file' do
@@ -106,15 +118,14 @@ module NcsNavigator
       end
 
       it 'is not mandatory' do
-        input_hash['Study Center'].delete 'sc_id'
-        lambda { from_hash }.
+        lambda { from_minimum_valid_hash }.
           should_not raise_error
       end
     end
 
     describe '#recruitment_type_id' do
       it 'reflects the configured value' do
-        from_hash.recruitment_type_id.should == '3'
+        everything.recruitment_type_id.should == '1'
       end
 
       it 'is always a string' do
@@ -123,8 +134,7 @@ module NcsNavigator
       end
 
       it 'is not mandatory' do
-        input_hash['Study Center'].delete 'recruitment_type_id'
-        lambda { from_hash }.
+        lambda { from_minimum_valid_hash }.
           should_not raise_error
       end
     end
@@ -178,62 +188,52 @@ module NcsNavigator
       end
 
       it 'is not required' do
-        input_hash['Study Center'].delete 'sampling_units_file'
-        lambda { from_hash }.
+        lambda { from_minimum_valid_hash }.
           should_not raise_error
       end
     end
 
     describe '#sampling_unit_areas' do
-      subject { everything.sampling_unit_areas }
-
-      it 'has the right number' do
-        subject.size.should == 2
+      describe 'when no sampling_unit_file' do
+        it 'is an empty array' do
+          from_minimum_valid_hash.sampling_unit_areas.should be_empty
+        end
       end
 
-      describe 'an individual area' do
-        let(:area) { subject.sort_by { |a| a.name }.last }
+      describe 'when sampling_units_file is present' do
+        subject { everything.sampling_unit_areas }
 
-        it 'has a name' do
-          area.name.should == 'Uptown'
+        it 'has the right number' do
+          subject.size.should == 2
         end
 
-        it 'has the right number of SSUs' do
-          area.secondary_sampling_units.size.should == 2
-        end
+        describe 'an individual area' do
+          let(:area) { subject.sort_by { |a| a.name }.last }
 
-        it 'has the right PSU' do
-          area.primary_sampling_unit.id.should == '204'
+          it 'has a name' do
+            area.name.should == 'Uptown'
+          end
+
+          it 'has the right number of SSUs' do
+            area.secondary_sampling_units.size.should == 2
+          end
+
+          it 'has the right PSU' do
+            area.primary_sampling_unit.id.should == '204'
+          end
         end
       end
     end
 
     describe '#primary_sampling_units' do
-      subject { everything.primary_sampling_units }
-
-      it 'has the right number' do
-        subject.size.should == 1
-      end
-
-      describe 'an individual PSU' do
-        let(:psu) { subject.first }
-
-        it 'has the correct ID' do
-          psu.id.should == '204'
-        end
-
-        it 'has the correct SSUs' do
-          psu.secondary_sampling_units.collect(&:id).should == %w(One Two Three)
+      describe 'when no sampling_unit_file' do
+        it 'is an empty array' do
+          from_minimum_valid_hash.primary_sampling_units.should be_empty
         end
       end
 
-      context 'without Areas and SSUs' do
-        before do
-          input_hash['Study Center']['sampling_units_file'] =
-            File.expand_path('../no_ssus.csv', __FILE__)
-        end
-
-        subject { from_hash.primary_sampling_units }
+      describe 'when sampling_units_file is present' do
+        subject { everything.primary_sampling_units }
 
         it 'has the right number' do
           subject.size.should == 1
@@ -246,102 +246,135 @@ module NcsNavigator
             psu.id.should == '204'
           end
 
-          it 'has no Areas' do
-            psu.should have(0).sampling_unit_areas
+          it 'has the correct SSUs' do
+            psu.secondary_sampling_units.collect(&:id).should == %w(One Two Three)
+          end
+        end
+
+        context 'without Areas and SSUs' do
+          before do
+            input_hash['Study Center']['sampling_units_file'] =
+              File.expand_path('../no_ssus.csv', __FILE__)
           end
 
-          it 'has no SSUs' do
-            psu.should have(0).secondary_sampling_units
+          subject { from_hash.primary_sampling_units }
+
+          it 'has the right number' do
+            subject.size.should == 1
+          end
+
+          describe 'an individual PSU' do
+            let(:psu) { subject.first }
+
+            it 'has the correct ID' do
+              psu.id.should == '204'
+            end
+
+            it 'has no Areas' do
+              psu.should have(0).sampling_unit_areas
+            end
+
+            it 'has no SSUs' do
+              psu.should have(0).secondary_sampling_units
+            end
           end
         end
       end
     end
 
     describe '#secondary_sampling_units' do
-      subject { everything.secondary_sampling_units }
-
-      it 'has the right number' do
-        subject.size.should == 3
-      end
-
-      describe 'an individual SSU' do
-        let(:ssu) { subject.sort_by { |ssu| ssu.id }.first }
-
-        it 'has the correct ID' do
-          ssu.id.should == 'One'
-        end
-
-        it 'has the correct name' do
-          ssu.name.should == 'West Side'
-        end
-
-        it 'has the correct area' do
-          ssu.area.name.should == 'Uptown'
-        end
-
-        it 'has the same area as another SSU in the same area' do
-          ssu.area.should eql(subject.find { |ssu| ssu.name == 'West Side' }.area)
-        end
-
-        it 'has the correct PSU' do
-          ssu.primary_sampling_unit.id.should == '204'
-        end
-
-        it 'has the correct TSUs' do
-          ssu.should have(1).tertiary_sampling_units
-        end
-
-        describe 'a TSU' do
-          let(:tsu) { ssu.tertiary_sampling_units.first }
-
-          it 'has a name' do
-            tsu.name.should == 'Center'
-          end
-
-          it 'has an ID' do
-            tsu.id.should == '1-1'
-          end
+      describe 'when no sampling_unit_file' do
+        it 'is an empty array' do
+          from_minimum_valid_hash.secondary_sampling_units.should be_empty
         end
       end
 
-      context 'without TSUs' do
-        before do
-          input_hash['Study Center']['sampling_units_file'] =
-            File.expand_path('../no_tsus.csv', __FILE__)
-        end
-
-        subject { from_hash.secondary_sampling_units }
+      describe 'when sampling_units_file is present' do
+        subject { everything.secondary_sampling_units }
 
         it 'has the right number' do
           subject.size.should == 3
         end
 
         describe 'an individual SSU' do
-          let(:ssu) { subject.first }
+          let(:ssu) { subject.sort_by { |ssu| ssu.id }.first }
 
-          it 'has no TSUs' do
-            ssu.should have(0).tertiary_sampling_units
+          it 'has the correct ID' do
+            ssu.id.should == 'One'
           end
-        end
-      end
 
-      context 'with extra whitespace' do
-        before do
-          input_hash['Study Center']['sampling_units_file'] =
-            File.expand_path('../spaces.csv', __FILE__)
-        end
-
-        subject { from_hash.secondary_sampling_units }
-
-        it 'has the right number' do
-          subject.size.should == 3
-        end
-
-        describe 'an individual SSU' do
-          let(:ssu) { subject.detect { |s| s.id == 'One' } }
-
-          it 'has the name' do
+          it 'has the correct name' do
             ssu.name.should == 'West Side'
+          end
+
+          it 'has the correct area' do
+            ssu.area.name.should == 'Uptown'
+          end
+
+          it 'has the same area as another SSU in the same area' do
+            ssu.area.should eql(subject.find { |ssu| ssu.name == 'West Side' }.area)
+          end
+
+          it 'has the correct PSU' do
+            ssu.primary_sampling_unit.id.should == '204'
+          end
+
+          it 'has the correct TSUs' do
+            ssu.should have(1).tertiary_sampling_units
+          end
+
+          describe 'a TSU' do
+            let(:tsu) { ssu.tertiary_sampling_units.first }
+
+            it 'has a name' do
+              tsu.name.should == 'Center'
+            end
+
+            it 'has an ID' do
+              tsu.id.should == '1-1'
+            end
+          end
+        end
+
+        context 'without TSUs' do
+          before do
+            input_hash['Study Center']['sampling_units_file'] =
+              File.expand_path('../no_tsus.csv', __FILE__)
+          end
+
+          subject { from_hash.secondary_sampling_units }
+
+          it 'has the right number' do
+            subject.size.should == 3
+          end
+
+          describe 'an individual SSU' do
+            let(:ssu) { subject.first }
+
+            it 'has no TSUs' do
+              ssu.should have(0).tertiary_sampling_units
+            end
+          end
+        end
+
+        context 'with extra whitespace' do
+          before do
+            input_hash['Study Center']['sampling_units_file'] =
+              File.expand_path('../spaces.csv', __FILE__)
+          end
+
+          subject { from_hash.secondary_sampling_units }
+
+          it 'has the right number' do
+            subject.size.should == 3
+          end
+
+          describe 'an individual SSU' do
+            let(:ssu) { subject.detect { |s| s.id == 'One' } }
+
+            it 'has the name' do
+              ssu.name.should == 'West Side'
+            end
           end
         end
       end
